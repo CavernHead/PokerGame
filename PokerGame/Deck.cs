4bc24f42cc518b6e31e5baee25fe06acc5c8cbbb
CavernHead <59891224+CavernHead@users.noreply.github.com>
2021-12-03 00:21:32 +0100

    initial commit

---
+++ PokerGame/Deck.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PokerGame
+{
+    class Deck
+    {
+        List<Card> deckCards;
+        
+        public Deck()
+        {
+            deckCards = new List<Card>();
+            for (int i = 0;i < Enum.GetNames(typeof(Card.eFamilly)).Length;i++)
+            {
+                for (int j = 2; j < 15; j++)
+                {
+                    deckCards.Add(new Card(j,((Card.eFamilly)i)));
+                }
+            }
+        }
+        public void shuffle()
+        {
+            for(int i = 0; i < deckCards.Count-1;i++)
+            {
+                int switchCard = Utillity.RandomNumber(i + 1, deckCards.Count);
+                switchToCardsPosition(i, switchCard);
+            }
+        }
+        public List<Card> drawNCards(int n)
+        {
+            List<Card> cardsRet = deckCards.GetRange(deckCards.Count - n, n);
+            deckCards.RemoveRange(deckCards.Count - n, n);
+            return cardsRet;
+        }
+        void switchToCardsPosition(int c1,int c2)
+        {
+            Card stored = deckCards[c1];
+            deckCards[c1] = deckCards[c2];
+            deckCards[c2] = stored;
+        }
+    }
+}
---
+++ PokerGame/Power.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PokerGame
+{
+    class Power
+    {
+        public enum eCombinationType
+        {
+            HighCard,
+            Pair,
+            TwoPair,
+            ThreeOfAkind,
+            Straight,
+            Flush,
+            FullHouse,
+            FourOfAkind,
+            StraightFlush,
+            RoyalFlush,
+        }
+        public List<Card> main;
+        public List<Card> kickers;
+        public eCombinationType comb;
+        public Power(List<Card> main, List<Card> kickers, eCombinationType comb)
+        {
+            this.main = main;
+            this.kickers = kickers;
+            this.comb = comb;
+        }
+        public Card getLastMain()
+        {
+            return main[main.Count - 1];
+        }
+        public Card getBeforeLastMain()
+        {
+            return main[main.Count - 2];
+        }
+        public Card GetKicker(int i)
+        {
+            return kickers[kickers.Count - i - 1];
+        }
+        public Card GetMain(int i)
+        {
+            return main[main.Count - 1-i];
+        }
+        public int CompareTo(Power pow)
+        {
+            int combStrThis = (int)comb;
+            int combStrThat = (int)pow.comb;
+            if (combStrThis == combStrThat)
+            {
+                switch(comb)
+                {
+                    case eCombinationType.HighCard:
+                        return CompareKickers(pow, GameMain.Q_TABLE_CARDS);
+
+                    case eCombinationType.Pair:
+                        return CompareStandard(pow, 3);
+
+                    case eCombinationType.TwoPair:
+                        return CompareStandard(pow, 1);
+
+                    case eCombinationType.ThreeOfAkind:
+                        return CompareStandard(pow, 2);
+
+                    case eCombinationType.Straight:
+                        return CompareStandard(pow, 0);
+                      
+                    case eCombinationType.Flush:
+                        return CompareStandard(pow, 0);
+                        
+                    case eCombinationType.FullHouse:
+                        return CompareStandard(pow, 0);
+                       
+                    case eCombinationType.FourOfAkind:
+                        return CompareStandard(pow, 1);
+                       
+                    case eCombinationType.StraightFlush:
+                        return CompareStandard(pow, 0);
+     
+                    case eCombinationType.RoyalFlush:
+                        return 0;  
+                }
+            }
+            else
+            if (combStrThis < combStrThat)
+            {
+                return -1;
+            }
+            else
+            {
+                return 1;
+            }
+            return 0;
+        }
+        int CompareKickers(Power pow,int ammount)
+        {
+            for (int i = 0; i < ammount; i++)
+            {
+                if (GetKicker(i).value == pow.GetKicker(i).value)
+                {
+                    continue;
+                }
+                else
+                if (GetKicker(i).value < pow.GetKicker(i).value)
+                {
+                    return -1;
+                }
+                else
+                {
+                    return 1;
+                }
+            }
+            return 0;
+        }
+        public int CompareStandard(Power pow, int amountOfKickersToCompare)
+        {
+            for(int i = 0; i< main.Count;i++)
+            {
+                if (GetMain(i).value == pow.GetMain(i).value)
+                {
+                    continue;
+                }
+                else
+                if (GetMain(i).value < pow.GetMain(i).value)
+                {
+                    return -1;
+                }
+                else
+                {
+                    return 1;
+                }
+            }
+            if(kickers==null)
+            {
+                return 0;
+            }
+            return CompareKickers(pow,amountOfKickersToCompare);
+        }
+    }
+}
